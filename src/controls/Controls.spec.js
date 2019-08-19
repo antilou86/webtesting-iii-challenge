87df80e41b69ea080f05e05d536d81887c9585a3
antilou86 <antilou86@gmail.com>
2019-08-19 17:59:14 -0400

    WIP: added testing to controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,7 +1,9 @@
 // Test away!
 import React from "react";
-import renderer from "react-test-renderer"; 
-import { render, fireEvent } from "@testing-library/reactyarn test";
+import renderer from "react-test-renderer";
+import "jest-dom/extend-expect";
+import { render, fireEvent } from "@testing-library/react";
+import "react-testing-library/cleanup-after-each";
 
 import Controls from './Controls'
 import Dashboard from '../dashboard/Dashboard'
@@ -11,10 +13,26 @@ describe('<Controls/>', () => {
         const tree = renderer.create(<Controls />).toJSON();
         expect(tree).toMatchSnapshot();
     })
-    it('toggles the closed/locked states when clicked', () => {
-        const toggleLock = Dashboard.toggleLocked();
-        const toggleClose = Dashboard.toggleClosed();
-        
-    })
-})
+    it("should open the gate on click", () => {
+        const { getByText } = render(<Dashboard />);
+        const open = getByText(/Close Gate/);
+        fireEvent.click(open);
+      });
+    it("should close gate", () => {
+        const { getByText } = render(<Dashboard />);
+        const lock = getByText(/Lock Gate/i);
+        fireEvent.click(lock);
+      });
+    it("should lock the gate", () => {
+        const { getByText } = render(<Dashboard />);
+        const close = getByText(/Close Gate/i);
+        const lock = getByText(/Lock Gate/i);
+    
+        fireEvent.click(close);
+        getByText(/Open Gate/i);
+    
+        fireEvent.click(lock);
+        getByText(/Unlock Gate/i);
+      });
+});
 
