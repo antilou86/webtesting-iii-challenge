c58d4a29c0c1f8ba39b3d2522d5bbc26f9832112
antilou86 <antilou86@gmail.com>
2019-08-19 17:07:06 -0400

    WIP: added dependencies and explored current code, also added package upgrades.  need to add some data-ids, for easier fetching during test.

--- src/controls/Controls.js
+++ src/controls/Controls.js
@@ -5,12 +5,15 @@ const Controls = props => {
 
   return (
     <div className="controls panel">
+
       <button disabled={!closed} onClick={toggleLocked} className="toggle-btn">
         {locked ? 'Unlock Gate' : 'Lock Gate'}
       </button>
+
       <button disabled={locked} onClick={toggleClosed} className="toggle-btn">
         {closed ? 'Open Gate' : 'Close Gate'}
       </button>
+      
     </div>
   );
 };
--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,20 @@
 // Test away!
+import React from "react";
+import renderer from "react-test-renderer"; 
+import { render, fireEvent } from "@testing-library/reactyarn test";
+
+import Controls from './Controls'
+import Dashboard from '../dashboard/Dashboard'
+
+describe('<Controls/>', () => {
+    it('matches the snapshot', () => {
+        const tree = renderer.create(<Controls />).toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+    it('toggles the closed/locked states when clicked', () => {
+        const toggleLock = Dashboard.toggleLocked();
+        const toggleClose = Dashboard.toggleClosed();
+        
+    })
+})
+
